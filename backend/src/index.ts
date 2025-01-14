b8ca48ae8562936dc7055c8ac96fdf5c0cd897b3
Musa Gillani <musagillaniwork@gmail.com>
2025-01-14 23:29:00 +0500

    backend setup with hono + bun

---
+++ backend/src/index.ts
@@ -0,0 +1,13 @@
+import { Hono } from 'hono'
+
+const app = new Hono()
+
+app.get('/', (c) => {
+  return c.text('Hello Hono!')
+})
+
+app.get('/health', (c) => {
+  return c.text('API healthy')
+})
+
+export default app
